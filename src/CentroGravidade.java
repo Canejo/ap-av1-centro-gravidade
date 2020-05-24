233a7cc221db5a3dd4656e46dc4b4fded7d2c848
Felipe Canejo <felipecanejo@gmail.com>
2020-05-24 16:42:39 -0300

    Calculando valores das colunas

--- src/CentroGravidade.java
+++ src/CentroGravidade.java
@@ -20,10 +20,9 @@ public class CentroGravidade {
           {0.4, 0.1, 0.1, 0.1, 0.2},
           {0.2, 0.2, 0.3, 0.3, 0.1},
         };
-        System.out.println("Hellow world");
         int[] cg = encontrarCentroGravidade(m);
         
-        System.out.printf("Centro ( %d, %d )", cg[0] + 1, cg[1] + 1);
+        System.out.printf("Centro ( %d, %d )\n", cg[0] + 1, cg[1] + 1);
     }
 
     public static int[] encontrarCentroGravidade(double[][] m) {
@@ -35,6 +34,7 @@ public class CentroGravidade {
         //Procurando gravidade das linhas        
         for (int i = 1; i < m.length - 1; i++) {
             double r = somarBlocosLinha(m, i);
+            //Verificando se é o menor valor ou se é o primeiro valor
             if (i == 1 || menorLinha > r) {
                 indiceLinha = i;
                 menorLinha = r;
@@ -42,7 +42,14 @@ public class CentroGravidade {
         }
         
         //Procurando gravidede das colunas
-        
+        for (int i = 1; i < m[0].length - 1; i++) {
+            double r = somarBlocosColuna(m, i);
+            //Verificando se é o menor valor ou se é o primeiro valor
+            if (i == 1 || menorColuna > r) {
+                indiceColuna = i;
+                menorColuna = r;
+            }
+        }
 
         return new int[] { indiceLinha, indiceColuna };
     }
@@ -51,12 +58,14 @@ public class CentroGravidade {
         double bloco1 = 0;
         double bloco2 = 0;
         
+        //Somando todos os valores das linhas no bloco 1
         for (int i = 0; i < indice; i++) {
             for (int j = 0; j < m[i].length; j++) {
                 bloco1 += m[i][j];
             }
         }
         
+        //Somando todos os valores das linhas no bloco 2
         for (int i = indice + 1; i < m.length; i++) {
             for (int j = 0; j < m[i].length; j++) {
                 bloco2 += m[i][j];
@@ -64,11 +73,36 @@ public class CentroGravidade {
         }        
 
         double r = bloco1 - bloco2;
-        
+        //Verificando se deu negativo e invertendo valor
         if (r < 0) {
             r *= -1;
         }
+        return r;
+    }
+    
+    public static double somarBlocosColuna(double[][] m, int indice) {
+        double bloco1 = 0;
+        double bloco2 = 0;
         
+        //Somando todos os valores das colunas no bloco 1
+        for (int i = 0; i < indice; i++) {
+            for (int j = 0; j < m.length; j++) {
+                bloco1 += m[j][i];
+            }
+        }
+        
+        //Somando todos os valores das colunas no bloco 2
+        for (int i = indice + 1; i < m[0].length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                bloco2 += m[j][i];
+            }
+        }
+        
+        //Verificando se deu negativo e invertendo valor
+        double r = bloco1 - bloco2;
+        if (r < 0) {
+            r *= -1;
+        }
         return r;
     }
 }
